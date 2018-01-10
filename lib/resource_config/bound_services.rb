89e150cc22eb9e95a9248dbd9154749da18ca83f
tom-collings <tom.collings@comcast.net>
2018-01-10 12:16:38 -0700

    testing for nil

--- lib/resource_config/bound_services.rb
+++ lib/resource_config/bound_services.rb
@@ -44,7 +44,7 @@ module ResourceConfig
 
         def services_as_resources(resources)
           @services.each do |service|
-            next unless (!service['includeInResources'].nil) && (service['includeInResources'].include? 'true')
+            next unless (service['credentials'].include? 'includeInResources') && (service['credentials']['includeInResources'] == 'true')
             add_resource service, resources
           end
         end
