393e3873ac705e256471b269e5c99363848ca7d2
tom-collings <tom.collings@comcast.net>
2018-01-10 08:20:26 -0700

    more ruby stuff

--- lib/resource_config/bound_services.rb
+++ lib/resource_config/bound_services.rb
@@ -4,58 +4,55 @@ require 'json'
 module ResourceConfig
 
   class BoundService
+    
 
-    class << self
+    attr_reader :environment
+    attr_reader :services
 
-      attr_reader :environment
-      attr_reader :services
+    def initialize(app_dir)
+      @app_dir = Pathname.new(File.expand_path(app_dir))
+      @environment = ENV.to_hash
+      @services    = Services.new(parse(@environment.delete('VCAP_SERVICES')))
+    end
 
-      def initialize(app_dir)
-        @app_dir = Pathname.new(File.expand_path(app_dir))
-        @environment = ENV.to_hash
-        @services    = Services.new(parse(@environment.delete('VCAP_SERVICES')))
-      end
+    def with_buildpack(app_dir)
+      initialize(app_dir)
+      supply
+    end
 
-      def with_buildpack(app_dir)
-        initialize(app_dir)
-        supply
-      end
+    def supply
+      mutate_resources_xml
+    end
 
-      def supply
-        mutate_resources_xml
-      end
+    def mutate_resources_xml
+        with_timing 'Modifying /WEB-INF/resources.xml for Resource Configuration' do
+          document = read_xml resources_xml
 
-      def mutate_resources_xml
-          with_timing 'Modifying /WEB-INF/resources.xml for Resource Configuration' do
-            document = read_xml resources_xml
+          resources  = REXML::XPath.match(document, '/resources').first
+          resources  = document.add_element 'resources' if resources.nil?
 
-            resources  = REXML::XPath.match(document, '/resources').first
-            resources  = document.add_element 'resources' if resources.nil?
+          write_xml resources_xml, document
 
-            write_xml resources_xml, document
-
-          end
-        end
-
-      def parse(input)
-          input ? JSON.parse(input) : {}
-      end
-
-      def resources_xml
-          @app_dir+ 'WEB-INF/resourcesXYZ.xml'
-      end
-
-      def read_xml(file)
-        File.open(file, 'a+') { |f| REXML::Document.new f }
-      end
-
-      def write_xml(file, document)
-        file.open('w') do |f|
-          formatter.write document, f
-          f << "\n"
         end
       end
 
+    def parse(input)
+        input ? JSON.parse(input) : {}
+    end
+
+    def resources_xml
+        @app_dir+ 'WEB-INF/resourcesXYZ.xml'
+    end
+
+    def read_xml(file)
+      File.open(file, 'a+') { |f| REXML::Document.new f }
+    end
+
+    def write_xml(file, document)
+      file.open('w') do |f|
+        formatter.write document, f
+        f << "\n"
+      end
     end
 
   end
