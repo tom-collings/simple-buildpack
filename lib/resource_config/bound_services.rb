d9a4cbca2a30257b5c8808f906afffdda72d5c38
tom-collings <tom.collings@comcast.net>
2018-01-10 15:30:19 -0700

    verifying formatter

--- lib/resource_config/bound_services.rb
+++ lib/resource_config/bound_services.rb
@@ -108,7 +108,7 @@ module ResourceConfig
 
       def formatter
         #formatter         = REXML::Formatters::Pretty.new(4)
-        formatter         = REXML::Formatters::Default.new
+        formatter         = REXML::Formatters::Transitive.new(4)
         #formatter.compact = true
         formatter
       end
